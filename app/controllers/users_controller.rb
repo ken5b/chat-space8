db40820e4643eefb4d39ab5a06a953f1bddebf41
Kengo Baba <kengo0316.giants@gmail.com>
2019-07-21 20:16:37 +0900

    インクリメンタルサーチ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,22 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def create
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def index
+    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
 
