519e5f5cd3dacea1829c778baa4a4a9e9a3f8685
Kengo Baba <kengo0316.giants@gmail.com>
2019-07-26 17:58:10 +0900

    tokyo

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
-      config.time_zone = 'Tokyo'
     end
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
